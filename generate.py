af01760f8c409e8ee44cb1ce2353dc728e475da9
Piper Welch <pwelch1@uvm.edu>
2023-01-20 12:15:40 -0500

    adding module joints

--- generate.py
+++ generate.py
@@ -1,16 +1,29 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
 length = 1
 width = 1
 height = 1
 x = 0
 y = 0
 z = 0.5
-for j in range(5):
-    for k in range(5):
-        for i in range(10):
-            pyrosim.Send_Cube(name="Box_{}".format(i), pos=[j,k,z+i], size=[length*0.9**i, width*0.9**i, height*0.9**i]) 
+def create_world():
+    pyrosim.Start_SDF("world.sdf")
+
+    pyrosim.Send_Cube(name="Box", pos=[x+30,y,z], size=[length, width, height]) 
 
 
-pyrosim.End()
+    pyrosim.End()
+    
+def create_robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[1,0,1.5], size=[length, width, height]) 
+    pyrosim.Send_Joint( name = "Torso_Backleg" , parent= "Torso" , child = "Backleg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name="Backleg", pos=[-0.5,0,-0.5], size=[length, width, height])
+    pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Torso" , child = "Frontleg" , type = "revolute", position = [1.5,0,1])
+    pyrosim.Send_Cube(name="Frontleg", pos=[0.5,0,-0.5], size=[length, width, height]) 
+    # pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0.5,0,1])
+    # pyrosim.Send_Cube(name="Link3", pos=[0.5,0,0.5], size=[length, width, height])
+    pyrosim.End()
+    
+create_world()
+create_robot()
--- simulate.py
+++ simulate.py
@@ -4,9 +4,10 @@ import pybullet_data
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8)
-planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+# p.setGravity(0,0,-9.8)
+# planeId = p.loadURDF("plane.urdf")
+# robotId = p.loadURDF("body.urdf")
+p.loadSDF("box.sdf")
 
 for i in range(1000):
     p.stepSimulation()
